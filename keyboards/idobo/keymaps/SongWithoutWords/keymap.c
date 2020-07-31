08d8b817318d1ca8538fb0465b3fb3bf6099f199
ian <ianmccall93@gmail.com>
2020-07-30 21:31:45 -0700

    [idobo] rename layers to simple numbering

--- keyboards/idobo/keymaps/SongWithoutWords/keymap.c
+++ keyboards/idobo/keymaps/SongWithoutWords/keymap.c
@@ -2,9 +2,9 @@
 
 
 enum Layer {
-  BASE,
-  SYMB,
-  MDIA,
+  L0, // Base
+  L1, // Symbols
+  L2, // Media
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -23,12 +23,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 '--------------------------------------------------------------------------------------------------------------------------------------'
 */
 
-[BASE] = LAYOUT_ortho_5x15(
+[L0] = LAYOUT_ortho_5x15(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH,
   KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_UP,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_LCTL, KC_GRV,  KC_QUOT, KC_LGUI, KC_LALT, KC_SPC,  MO(SYMB),MO(MDIA),MO(SYMB),KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_RCTL
+  KC_LCTL, KC_GRV,  KC_QUOT, KC_LGUI, KC_LALT, KC_SPC,  MO(L1),  MO(L2),  MO(L1),KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_RCTL
 ),
 
 /* SYMBOLS
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '-----------------------------------------------------------------------------------------------------------------------------------------------------'
  */
 
-[SYMB] = LAYOUT_ortho_5x15(
+[L1] = LAYOUT_ortho_5x15(
 
 // .-----------------------------------------------------------------------------------------------------------------------------------------------------.
      _______ , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , _______ , _______ , _______ , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_DEL  ,
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
-[MDIA] = LAYOUT_ortho_5x15(
+[L2] = LAYOUT_ortho_5x15(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
   KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, \
   KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, RESET,   _______, _______, _______, \
